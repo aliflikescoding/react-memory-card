4cd94b16b6d0402fea16f65fad806ea913355132
Alif Wibowo <alif.likes.coding@gmail.com>
2024-06-03 18:50:17 +0700

    Modify footer flex

--- app/src/sections/Footer.jsx
+++ app/src/sections/Footer.jsx
@@ -4,7 +4,7 @@ import { FaGithub } from "react-icons/fa";
 const Footer = () => {
   return (
     <footer className="flex justify-center items-center">
-      <a className="flex text-1xl sm:text-4xl font-semibold tracking-wider" href="https://github.com/aliflikescoding" target="_">aliflikescoding <FaGithub className="ml-[5px]"/></a>
+      <a className="flex justify-center items-center text-1xl sm:text-4xl font-semibold tracking-wider" href="https://github.com/aliflikescoding" target="_">aliflikescoding <FaGithub className="ml-[5px]"/></a>
     </footer>
   )
 }
