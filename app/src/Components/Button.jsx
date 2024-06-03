669417ddab6b7be3c1589246c541bea38d7ee8e3
Alif Wibowo <alif.likes.coding@gmail.com>
2024-06-03 18:49:37 +0700

    Make button component

---
+++ app/src/Components/Button.jsx
@@ -0,0 +1,39 @@
+import { MotionConfig, motion } from "framer-motion"
+
+const Button = ({
+  label,
+  iconComponent,
+  backgroundColor,
+  textColor,
+  borderColor,
+  fullWidth,
+}) => {
+  return (
+    <MotionConfig
+    transition={{
+      duration: '0.125',
+      ease: 'easeInOut'
+    }}
+    >
+      <motion.button className={`flex justify-center items-center gap-1 sm:gap-2 px-4 py-3 sm:px-6 sm:py-3 font-opensans text-base sm:text-lg leading-none capitalize font-semibold
+      ${
+        backgroundColor
+          ? `${backgroundColor} ${textColor} ${borderColor}`
+          : "bg-primary text-text"
+      } rounded-full ${fullWidth && "w-full"}`}
+      whileHover={{
+        scale: 1.05,
+      }}
+      whileTap={{
+        scale: 0.95,
+        rotate: "2.5deg",
+      }}
+      >
+        {label}
+        {iconComponent}
+      </motion.button>
+    </MotionConfig>
+  )
+}
+
+export default Button
