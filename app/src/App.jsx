501f098c11d2adb28759d290087259c6cdf6145b
Alif Wibowo <alif.likes.coding@gmail.com>
2024-06-03 16:53:02 +0700

    Add layout basics

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,7 +1,17 @@
 function App() {
   return (
     <>
-      
+      <main className="relative bg-background text-text font-opensans">
+        <section className="padding-x">
+          Header
+        </section>
+        <section className="padding-x">
+          Main Area
+        </section>
+        <section className="padding-x">
+          Footer
+        </section>
+      </main>
     </>
   )
 }
