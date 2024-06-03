eabd6937591b3646d89822043a342c36f930c709
Alif Wibowo <alif.likes.coding@gmail.com>
2024-06-03 16:44:03 +0700

    Modify taliwind config

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -3,7 +3,30 @@ export default {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   content: [],
   theme: {
-    extend: {},
+    fontSize: {
+      xs: ['12px', '16px'],
+      sm: ['14px', '20px'],
+      base: ['16px', '19.5px'],
+      lg: ['18px', '21.94px'],
+      xl: ['20px', '24.38px'],
+      '2xl': ['24px', '29.26px'],
+      '3xl': ['28px', '50px'],
+      '4xl': ['48px', '58px'],
+      '8xl': ['96px', '106px']
+    },
+    extend: {
+      fontFamily: {
+        opensans: ['Open Sans', 'sans-serif'],
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
+      colors: {
+        'primary': "#ffcb85",
+        "background": "#071c39",
+        "text": "#ffffff",
+        "error": "#FF8981",
+        "success": "#4F9C4E"
+      },
+    },
   },
   plugins: [],
 }
