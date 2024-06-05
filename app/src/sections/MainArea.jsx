3ed248f5bbe29d463cf40211ab5b0e6540af77e6
Alif Wibowo <alif.likes.coding@gmail.com>
2024-06-06 04:37:25 +0700

    Add basic game UI

--- app/src/sections/MainArea.jsx
+++ app/src/sections/MainArea.jsx
@@ -3,6 +3,8 @@ import { FaPlayCircle } from "react-icons/fa";
 import { useEffect, useState } from "react";
 import InfoModal from "../Components/InfoModal";
 import MovieCard from "../Components/MovieCard";
+import { MdOutlineRestartAlt } from "react-icons/md";
+
 
 const MainArea = () => {
   const [start, setStart] = useState(false);
@@ -27,9 +29,13 @@ const MainArea = () => {
     }, [start])
 
   return (
-    <div className="h-[89vh] sm:h-[79.7vh] overflow-auto font-bold p-5">
+    <div className="h-[89vh] sm:h-[100vh] font-bold p-5 overflow-auto">
       {start ? 
         <>
+          <div className="flex justify-center items-center flex-col">
+            <h1 className="text-2xl py-4">Current Score: 1 Max Score : 50</h1>
+            <Button label="Restart" className="py-4" backgroundColor="bg-error" clickEvent={handleStartClick} iconComponent={<MdOutlineRestartAlt />}/>
+          </div>
           <div className="flex flex-wrap flex-1 justify-center">
             {movieList.map((movie, index) => (
               <MovieCard key={index} movieLink={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} movieTitle={movie.original_title}/>
