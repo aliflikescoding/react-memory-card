ffc23f5cd655d08fa98e0d4656503cf004bb0a62
Alif Wibowo <alif.likes.coding@gmail.com>
2024-06-06 05:51:29 +0700

    Add game logic

--- app/src/sections/MainArea.jsx
+++ app/src/sections/MainArea.jsx
@@ -10,13 +10,18 @@ const MainArea = () => {
   const [start, setStart] = useState(false);
   const [movieList, setMovieList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [clickedList, setClickedList] = useState([]);
+  const [moviePosition, setMoviePosition] = useState(null);
+  const [currentScore, setCurrentScore] = useState(0);
+  const [maxScore, setMaxScore] = useState(0);
 
   const getMovie = async () => {
     setIsLoading(true); // Set loading state to true before fetching data
     try {
       const response = await fetch("https://api.themoviedb.org/3/discover/movie?api_key=3cd7ce9c3d0c6c0b6e837006a8d21a1e");
       const json = await response.json();
-      setMovieList(json.results);
+      const shuffledMovieList = shuffleArray(json.results);
+      setMovieList(shuffledMovieList);
     } catch (error) {
       console.error('Error fetching movie data:', error);
     } finally {
@@ -24,18 +29,58 @@ const MainArea = () => {
     }
   }
 
+  const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+
   const handleStartClick = () => {
     setStart(true);
   }
 
   const handleRestartClick = () => {
-    setStart(!start);
+    restartGame();
   }
+
+  const handleMovieCardClick = (movieTitle) => {
+    if (moviePosition == movieTitle) {
+      restartGame();
+    }
+    else {
+      setMoviePosition(movieTitle);
+    }
+  }
+
+  const restartGame = () => {
+    setStart(!start);
+    setClickedList([]);
+    setMoviePosition(null);
+    if (currentScore > maxScore) {
+      setMaxScore(currentScore);
+    }
+    setCurrentScore(0);
+  }
+
+  useEffect(() => {
+    if (moviePosition !== null) {
+      if (clickedList.includes(moviePosition)) {
+        restartGame();
+      }
+      else {
+        setCurrentScore(currentScore+1);
+        setMovieList(shuffleArray(movieList));
+        setClickedList((prevClickedList) => [...prevClickedList, moviePosition]);
+      }
+    }
+    console.log(clickedList);
+  }, [moviePosition]);
 
   useEffect(() => {
       if (start === true) {
         getMovie();
-        console.log(movieList);
       }
     }, [start]);
 
@@ -44,7 +89,7 @@ const MainArea = () => {
       {start ? 
         <>
           <div className="flex justify-center items-center flex-col">
-            <h1 className="text-2xl py-4">Current Score: 1 Max Score : 50</h1>
+            <h1 className="text-2xl py-4">Current Score: {currentScore} Max Score : {maxScore}</h1>
             <Button label="Restart" className="py-4" backgroundColor="bg-error" clickEvent={handleRestartClick} iconComponent={<MdOutlineRestartAlt />}/>
           </div>
           { isLoading ?
@@ -53,7 +98,7 @@ const MainArea = () => {
             </div> :
             <div className="flex flex-wrap flex-1 justify-center">
               {movieList.map((movie, index) => (
-                <MovieCard key={index} movieLink={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} movieTitle={movie.title}/>
+                <MovieCard key={index} movieLink={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} movieTitle={movie.title} handleClick={() => handleMovieCardClick(movie.title)}/>
               ))}
             </div>
           }
@@ -61,7 +106,8 @@ const MainArea = () => {
         : 
         <div className="flex flex-col justify-center items-center">
           <h1 className="text-6xl my-8">Play Now!</h1>
-          <Button label="Begin " clickEvent={handleStartClick} iconComponent={<FaPlayCircle />}/>
+          {maxScore > 0 && <h1 className="text-3xl py-8">Your Current Max Score : {maxScore}</h1>}
+          <Button label={`${maxScore > 0 ? "Play Again" : "Begin"}`} clickEvent={handleStartClick} iconComponent={<FaPlayCircle />}/>
           <InfoModal />
         </div>}
       
